f17e8b0e0403c47fde9273315a6a8b98988813eb
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-04-10 12:09:39 -0300

    refactor: exercise controller

---
+++ backend/app/Http/Controllers/Api/ExerciseControllers.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Exercise;
+use App\Http\Requests\ExercisePostRequest;
+use App\Traits\ApiResponse;
+use Exception;
+
+class ExerciseControllers extends Controller
+{
+    use ApiResponse;
+    public function index()
+    {
+        try {
+            $exercises = Exercise::all();
+            return $this->success(200, '¡Lista de ejercicios!', $exercises);
+        } catch (Exception $e) {
+            return $this->error(404, 'Error al obtener los ejercicios.');
+        }
+    }
+
+    public function store(ExercisePostRequest $request)
+    {
+        try {
+            $exercise = Exercise::create($request->all());
+            $exercise->save(); 
+            return $this->success(201, '¡Ejercicio creado exitosamente!', $exercise);
+        } catch (Exception $e) {
+            return $this->error(404, 'Error al crear el ejercicio.');
+        }
+    }
+
+    public function show(Exercise $id)
+    {
+        try {
+            return $this->success(200, '¡Ejercicio obtenido exitosamente!', $id);
+        } catch (Exception $e) {
+            return $this->error(404, 'Error al obtener el ejercicio.');          
+        }
+    }
+
+    public function update(ExercisePostRequest $request, $exercise)
+    {
+        try {
+            $exercise = Exercise::find($exercise);
+            $exercise->update($request->only('name','description','image_uri','break','muscle_group'));  
+            return $this->success(200, '¡Ejercicio actualizado exitosamente!', $exercise);
+        } catch (Exception $e) {
+            return $this->error(404, 'Error al actualizar el ejercicio.');          
+        }
+    }
+
+    public function destroy($exercise)
+    {
+        try {
+            $exercise = Exercise::find($exercise);
+            $exercise->delete();
+            return $this->success(200, '¡Ejercicio eliminado exitosamente!', $exercise);
+        } catch (Exception $e) {
+            return $this->error(404, 'Error al actualizar el ejercicio.');          
+        }
+    }
+}
