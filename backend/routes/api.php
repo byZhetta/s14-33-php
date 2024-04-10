018d2b3c1db7bd07d813daa43f07ff3fe5838d3b
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-04-10 14:55:40 -0300

    refactor: exercise routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,20 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
-
-
+use App\Http\Controllers\Api\ExerciseControllers;
 
 
 Route::post('register', [AuthController::class, 'register'])->name('api.register');
-
 Route::post('login', [AuthController::class, 'login'])->name('api.login');
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('api.logout');
-
-    //rutas de exercises         //en el Postman http://127.0.0.1:8000/api/exercises
-Route::get('/exercises', 'App\Http\Controllers\ExerciseControllers@index');
-Route::post('/exercises', 'App\Http\Controllers\ExerciseControllers@store');
-Route::get('/exercises/{id}', 'App\Http\Controllers\ExerciseControllers@show');
-Route::put('/exercises/{exercise}', 'App\Http\Controllers\ExerciseControllers@update');
-Route::delete('/exercises/{exercise}', 'App\Http\Controllers\ExerciseControllers@destroy');
+    
+    // Rutas de exercises
+    Route::get('exercises', [ExerciseControllers::class, 'index'])->name('exercise.index');
+    Route::post('exercises', [ExerciseControllers::class, 'store'])->name('exercise.store');
+    Route::get('exercises/{id}', [ExerciseControllers::class, 'show'])->name('exercise.show');
+    Route::put('exercises/{exercise}', [ExerciseControllers::class, 'update'])->name('exercise.update');
+    Route::delete('exercises/{exercise}', [ExerciseControllers::class, 'destroy'])->name('exercise.destroy');
 });
