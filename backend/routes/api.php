1b2062a470e91b9c35c323bdb33b8fad009fcad3
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-04-15 13:37:26 -0300

    feat: objective routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\ExerciseControllers;
+use App\Http\Controllers\Api\ObjectiveController;
 use App\Http\Controllers\Api\ProfileController;
 
 Route::post('register', [AuthController::class, 'register'])->name('api.register');
@@ -19,4 +20,9 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('exercises/{id}', [ExerciseControllers::class, 'show'])->name('exercise.show');
     Route::put('exercises/{exercise}', [ExerciseControllers::class, 'update'])->name('exercise.update');
     Route::delete('exercises/{exercise}', [ExerciseControllers::class, 'destroy'])->name('exercise.destroy');
+    
+    // Rutas de objectives
+    Route::post('objectives', [ObjectiveController::class, 'store'])->name('objective.store');
+    Route::get('objectives', [ObjectiveController::class, 'show'])->name('objective.show');
+    Route::put('objectives', [ObjectiveController::class, 'update'])->name('objective.update');
 });
