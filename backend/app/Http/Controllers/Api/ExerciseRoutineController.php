1b92a4cd8f881c3c4735c6ce217bf9baa7b5c2cd
Ignacio <47858291+byZhetta@users.noreply.github.com>
2024-04-19 16:39:27 -0300

    refactor: exercise routine controller

--- backend/app/Http/Controllers/Api/ExerciseRoutineController.php
+++ backend/app/Http/Controllers/Api/ExerciseRoutineController.php
@@ -1,83 +1,88 @@
 <?php
 
 namespace App\Http\Controllers\Api;
+
 use App\Http\Controllers\Controller;
 use App\Models\ExerciseRoutine;
+use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Exception;
 
 class ExerciseRoutineController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+    use ApiResponse;
     public function index()
     {
-        $relation= ExerciseRoutine::all();
-        return response()->json($relation,200);
-
+        try {
+            $relation= ExerciseRoutine::all();
+            return $this->success(200, 'Rutina de ejercicios', $relation);
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al mostrar la rutina de ejercicios.');
+        }
     }
-
 
     public function store(Request $request)
     {
-        $relation = ExerciseRoutine::create($request->all());
-        return response()->json([
-            'success'=>true,
-            'data'=>$relation
-        ],201);
+        try {
+            $relation = ExerciseRoutine::create($request->all());
+            return $this->success(201, 'Rutina de ejercicios', $relation);
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al crear la rutina de ejercicios.');
+        }
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(string $id)
     {
-        $relation = ExerciseRoutine::find($id);
-        return response()->json($relation,200);
+        try {
+            $relation = ExerciseRoutine::find($id);
+            return $this->success(200, 'Rutina de ejercicios', $relation);
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al mostrar la rutina de ejercicios.');
+        }
     }
-
 
     public function update(Request $request, string $id)
     {
-        $relation= ExerciseRoutine::find($id);
-        $relation->exercise_id= $request->exercise_id;
-        $relation->routine_id=$request->routine_id;
-        $relation->day= $request->day;
-        $relation->series= $request->series;
-        $relation->repetition= $request->repetition;
-        $relation->weight= $request->weight;
-        $relation->complete= $request->complete;
-        $relation->save();
-
-        return response()->json([
-            'success'=>true,
-            'data'=>$relation
-        ],200);
+        try {
+            $relation= ExerciseRoutine::find($id);
+            $relation->exercise_id= $request->exercise_id;
+            $relation->routine_id=$request->routine_id;
+            $relation->day= $request->day;
+            $relation->series= $request->series;
+            $relation->repetition= $request->repetition;
+            $relation->weight= $request->weight;
+            $relation->complete= $request->complete;
+            $relation->save();
+    
+            return $this->success(200, 'Rutina de ejercicios', $relation);
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al actualizar la rutina de ejercicios.');
+        }
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(string $id)
     {
-        ExerciseRoutine::find($id)->delete();
-        return response()->json(['success'=>true],200);
+        try {
+            ExerciseRoutine::find($id)->delete();
+            return $this->success(200, 'Rutina de ejercicios eliminada');
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al eliminar la rutina de ejercicios.');
+        }
     }
 
-
     public function complete(string $id){
-        $relation =ExerciseRoutine::find($id);
-        if($relation->complete){
-            $relation->complete=false;
-        }else{
-            $relation->complete=true;
+        try {
+            $relation =ExerciseRoutine::find($id);
+            if($relation->complete){
+                $relation->complete=false;
+            }else{
+                $relation->complete=true;
+            }
+            $relation->save();    
+
+            return $this->success(200, 'Rutina de ejercicios', $relation);
+        } catch (Exception $e) {
+            return $this->error(500, 'Error al completar el ejercicio.');
         }
-
-        $relation->save();
-
-        return response()->json([
-            'success'=>true,
-            'data'=>$relation
-        ],200);
     }
 }
