045cfb79bef77e9f89c9bf731e0cc7fd72fac8bf
Emanuel Ivan Robledo <emanuel.robledo@hotmail.com>
2024-04-19 23:41:20 -0300

    feat:goals

--- frontend/src/Pages/Objetivos/Objetivos.jsx
+++ frontend/src/Pages/Objetivos/Objetivos.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Card from '../../components/Objetivos/Card/card.jsx';
+import Buttons from '../../components/Objetivos/Buttons/buttons.jsx';
+import Buttons2 from '../../components/Objetivos/Buttons/buttons2.jsx';
+import ButtonSave from '../../components/Objetivos/Buttons/buttonSave.jsx';
 
 const Objetivos = () => {
   return (
-    <div>Objetivos</div>
-  )
-}
+    <section className="bg-[#131429] min-h-screen flex flex-col justify-start items-center text-white pt-9 px-3 lg:pl-28">
+      <Card />
+      <Buttons />
+      <Buttons2 />
+      <ButtonSave />
+    </section>
+  );
+};
 
 export default Objetivos;
